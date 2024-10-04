b833e190a509f3bf4d77bf3ec5881520add0ddac
bellandry <bellandry.work@gmail.com>
2024-10-04 16:53:05 +0100

    add reset and submit actions

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,35 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import { SWATCHES } from "@/constants";
+import axios from "axios";
 import { useEffect, useRef, useState } from "react";
+
+interface Response {
+  expr: string;
+  result: string;
+  assign: boolean;
+}
+
+interface GeneralResult {
+  expression: string;
+  answer: string;
+}
 
 export default function Home() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const [isDrawing, setIsDrawing] = useState(false);
+  const [color, setColor] = useState("rgb(255, 255, 255)");
+  const [reset, setReset] = useState(false);
+  const [result, setResult] = useState<GeneralResult | null>(null);
+  const [dictOfVars, setDictOfVars] = useState({});
+
+  useEffect(() => {
+    if (reset) {
+      resetCanvas();
+      setReset(false);
+    }
+  }, [reset]);
 
   useEffect(() => {
     const canvas = canvasRef.current;
@@ -43,15 +68,59 @@ export default function Home() {
     if (canvas) {
       const ctx = canvas.getContext("2d");
       if (ctx) {
-        ctx.strokeStyle = "white";
+        ctx.strokeStyle = color;
         ctx.lineTo(e.nativeEvent.offsetX, e.nativeEvent.offsetY);
         ctx.stroke();
       }
     }
   };
 
+  const resetCanvas = () => {
+    const canvas = canvasRef.current;
+    if (canvas) {
+      const ctx = canvas.getContext("2d");
+      if (ctx) {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+      }
+    }
+  };
+
+  const sendData = async () => {
+    const canvas = canvasRef.current;
+    if (canvas) {
+      const response = await axios({
+        method: "POST",
+        url: `${process.env.NEXT_PUBLIC_API_URL}/calculate`,
+        data: {
+          image: canvas.toDataURL("image/png"),
+          dict_of_vars: dictOfVars,
+        },
+      });
+
+      const resp = await response.data;
+      console.log(resp);
+    }
+  };
+
   return (
     <>
+      <div className="flex justify-between container mx-auto pt-4">
+        <Button
+          variant={"outline"}
+          onClick={() => setReset(true)}
+          className="z-50"
+        >
+          Reset
+        </Button>
+        <div className="z-50">
+          {SWATCHES.map((color) => {
+            return color;
+          })}
+        </div>
+        <Button variant={"outline"} onClick={sendData} className="z-50">
+          Calculer
+        </Button>
+      </div>
       <canvas
         ref={canvasRef}
         id="canvas"
