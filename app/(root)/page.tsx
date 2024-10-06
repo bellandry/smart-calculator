90f756624c68530cef6875931422dcb7107367eb
bellandry <bellandry.work@gmail.com>
2024-10-06 18:22:22 +0100

    setup swatces palet & eraser

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -23,6 +23,8 @@ export default function Home() {
   const [reset, setReset] = useState(false);
   const [result, setResult] = useState<GeneralResult | null>(null);
   const [dictOfVars, setDictOfVars] = useState({});
+  const [eraserSize, setEraserSize] = useState(10); // Ajout de l'état pour la taille de la gomme
+  const [isErasing, setIsErasing] = useState(false); // Ajout de l'état pour savoir si on utilise la gomme
 
   useEffect(() => {
     if (reset) {
@@ -44,14 +46,23 @@ export default function Home() {
     }
   }, []);
 
-  const startDrawing = (e: React.MouseEvent<HTMLCanvasElement>) => {
+  const startDrawing = (
+    e: React.MouseEvent<HTMLCanvasElement> | React.TouchEvent<HTMLCanvasElement>
+  ) => {
     const canvas = canvasRef.current;
     if (canvas) {
       canvas.style.background = "black";
       const ctx = canvas.getContext("2d");
       if (ctx) {
         ctx.beginPath();
-        ctx.moveTo(e.nativeEvent.offsetX, e.nativeEvent.offsetY);
+        if ("offsetX" in e.nativeEvent) {
+          ctx.moveTo(e.nativeEvent.offsetX, e.nativeEvent.offsetY);
+        } else {
+          ctx.moveTo(
+            e.nativeEvent.touches[0].clientX - canvas.offsetLeft,
+            e.nativeEvent.touches[0].clientY - canvas.offsetTop
+          );
+        }
         setIsDrawing(true);
       }
     }
@@ -61,16 +72,48 @@ export default function Home() {
     setIsDrawing(false);
   };
 
-  const draw = (e: React.MouseEvent<HTMLCanvasElement>) => {
+  const draw = (
+    e: React.MouseEvent<HTMLCanvasElement> | React.TouchEvent<HTMLCanvasElement>
+  ) => {
     if (!isDrawing) return;
 
     const canvas = canvasRef.current;
     if (canvas) {
       const ctx = canvas.getContext("2d");
       if (ctx) {
-        ctx.strokeStyle = color;
-        ctx.lineTo(e.nativeEvent.offsetX, e.nativeEvent.offsetY);
-        ctx.stroke();
+        if (isErasing) {
+          // Vérifie si l'outil est la gomme
+          if ("offsetX" in e.nativeEvent) {
+            ctx.clearRect(
+              e.nativeEvent.offsetX - eraserSize / 2,
+              e.nativeEvent.offsetY - eraserSize / 2,
+              eraserSize,
+              eraserSize
+            );
+          } else {
+            ctx.clearRect(
+              e.nativeEvent.touches[0].clientX -
+                canvas.offsetLeft -
+                eraserSize / 2,
+              e.nativeEvent.touches[0].clientY -
+                canvas.offsetTop -
+                eraserSize / 2,
+              eraserSize,
+              eraserSize
+            );
+          }
+        } else {
+          ctx.strokeStyle = color;
+          if ("offsetX" in e.nativeEvent) {
+            ctx.lineTo(e.nativeEvent.offsetX, e.nativeEvent.offsetY);
+          } else {
+            ctx.lineTo(
+              e.nativeEvent.touches[0].clientX - canvas.offsetLeft,
+              e.nativeEvent.touches[0].clientY - canvas.offsetTop
+            );
+          }
+          ctx.stroke();
+        }
       }
     }
   };
@@ -102,9 +145,13 @@ export default function Home() {
     }
   };
 
+  const toggleEraser = () => {
+    setIsErasing(!isErasing); // Fonction pour basculer entre le dessin et l'effacement
+  };
+
   return (
     <>
-      <div className="flex justify-between container mx-auto pt-4">
+      <div className="flex justify-between container mx-auto pt-4 px-2">
         <Button
           variant={"outline"}
           onClick={() => setReset(true)}
@@ -112,9 +159,31 @@ export default function Home() {
         >
           Reset
         </Button>
-        <div className="z-50">
+        <Button
+          variant={"outline"}
+          onClick={toggleEraser} // Appel de la fonction toggleEraser
+          className="z-50"
+        >
+          {isErasing ? "Dessin" : "Gomme"}{" "}
+          {/* Bouton pour activer/désactiver la gomme */}
+        </Button>
+        <input
+          type="range"
+          min="1"
+          max="100"
+          value={eraserSize}
+          onChange={(e) => setEraserSize(Number(e.target.value))} // Contrôle de la taille de la gomme
+          className="z-50"
+        />
+        <div className="z-50 absolute top-16 flex-col md:top-0 md:flex-row md:relative flex items-center justify-center gap-2 p-2 rounded-md bg-neutral-900">
           {SWATCHES.map((color) => {
-            return color;
+            return (
+              <div
+                className={`h-6 w-6 bg-[${color}] rounded-full cursor-pointer`}
+                key={color}
+                onClick={() => setColor(color)}
+              />
+            );
           })}
         </div>
         <Button variant={"outline"} onClick={sendData} className="z-50">
@@ -124,12 +193,15 @@ export default function Home() {
       <canvas
         ref={canvasRef}
         id="canvas"
-        className="absolute top-0 left-0 w-screen h-screen"
+        className="absolute top-0 left-0 w-full h-full"
         onMouseDown={startDrawing}
+        onTouchStart={startDrawing}
         onMouseOut={stopDrawing}
         onMouseUp={stopDrawing}
+        onTouchEnd={stopDrawing}
         onMouseMove={draw}
-      ></canvas>
+        onTouchMove={draw}
+      />
     </>
   );
 }
